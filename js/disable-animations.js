087d984ae986f4c268da6712b4bfde4dc33c3df7
iKollor <isai_aulayvida@hotmail.com>
2022-09-21 22:22:36 -0500

    icon toggle animation changed, noticias href link

--- js/disable-animations.js
+++ js/disable-animations.js
@@ -1,6 +1,7 @@
 let toggleAnimCheck = document.getElementById("toggleAnimCheck");
 const texts = document.querySelectorAll(".type");
 const btn = document.querySelector(".toggleAnimButton");
+const icon = document.querySelector("#icon");
 
 btn.addEventListener("click", () => {
   for (let i = 0; i < texts.length; i++) {
@@ -8,9 +9,13 @@ btn.addEventListener("click", () => {
     if (toggleAnimCheck.checked) {
       text.classList.toggle("toggleAnim");
       text.classList.toggle("text-visible");
+      icon.classList.add("fa-stop");
+      icon.classList.remove("fa-play");
     } else {
       text.classList.toggle("toggleAnim");
       text.classList.toggle("text-visible");
+      icon.classList.add("fa-play");
+      icon.classList.remove("fa-stop");
     }
   }
 });
--- js/dist/disable-animations.dev.js
+++ js/dist/disable-animations.dev.js
@@ -3,6 +3,7 @@
 var toggleAnimCheck = document.getElementById("toggleAnimCheck");
 var texts = document.querySelectorAll(".type");
 var btn = document.querySelector(".toggleAnimButton");
+var icon = document.querySelector("#icon");
 btn.addEventListener("click", function () {
   for (var i = 0; i < texts.length; i++) {
     var text = texts[i];
@@ -10,9 +11,13 @@ btn.addEventListener("click", function () {
     if (toggleAnimCheck.checked) {
       text.classList.toggle("toggleAnim");
       text.classList.toggle("text-visible");
+      icon.classList.add("fa-stop");
+      icon.classList.remove("fa-play");
     } else {
       text.classList.toggle("toggleAnim");
       text.classList.toggle("text-visible");
+      icon.classList.add("fa-play");
+      icon.classList.remove("fa-stop");
     }
   }
 });
